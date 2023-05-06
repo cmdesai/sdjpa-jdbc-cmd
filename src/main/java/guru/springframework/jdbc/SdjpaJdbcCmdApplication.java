46d763c47979d7c3d4ddfe09470aa8552cb68a2e
cmdesai <cmdesai@hotmail.com>
2023-05-06 23:51:54 +0530

    initial project

---
+++ src/main/java/guru/springframework/jdbc/SdjpaJdbcCmdApplication.java
@@ -0,0 +1,11 @@
+package guru.springframework.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaJdbcCmdApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SdjpaJdbcCmdApplication.class, args);
+    }
+}
